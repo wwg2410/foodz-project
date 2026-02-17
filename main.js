84cc9983aba56e43e2ec03c535fcc2f2fc4ca2c9
Samir <follinday@gmail.com>
2026-02-17 20:22:17 +0600

    add popup

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+
 let burger = document.querySelector(".header__burger");
 let menu = document.querySelector('.header__right');
 
-const clickBurger = ()=>{
+const clickBurger = () => {
     menu.classList.toggle('active');
 }
+
+
+
+
+
+let form = document.querySelector(".form");
+
+const clickPopup = () => {
+  form.classList.toggle("active");
+};
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+
+let form__success= document.querySelector(".form__success");
+
+const clickPopup1 = () => {
+    form.classList.remove('active')
+
+    form__success.classList.toggle("active");
+};
+
+form__success.addEventListener("click", (e) => {
+  e.preventDefault();
+});
